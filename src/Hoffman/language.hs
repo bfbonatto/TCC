3b6af0aacc2bbb70d8c93f517bfc16e04aab1950
Bruno Bonatto <bfbonatto@gmail.com>
2021-01-04 07:42:57 -0300

    removed arbitrary functions from the language, now only through top-level declarations

--- src/Hoffman/language.hs
+++ src/Hoffman/language.hs
@@ -9,7 +9,6 @@ data Term = TBool Bool
 			| TOp Term (Int -> Int -> Int) Term
 			| TIf Term Term Term
 			| TVar Var
-			-- | TAbs Var Type Term
 			| TApp Term Term
 			| TEmpty
 			| TList Term Term
@@ -17,45 +16,46 @@ data Term = TBool Bool
 
 type Var = String
 
+type Declarations = Map.Map Var Value
 
 data Value = VNum Int | VBool Bool | VFun Var Term | VEmpty | VList Value Value
 
 
-bigstep :: Term -> Maybe Value
-bigstep (TBool b) = Just (VBool b)
-bigstep (TNum n) = Just (VNum n)
+bigstep :: Term -> Declarations -> Maybe Value
+bigstep (TBool b) _ = Just (VBool b)
+bigstep (TNum n) _ = Just (VNum n)
 
-bigstep (TOp t1 op t2) = do
-	VNum n1 <- bigstep t1
-	VNum n2 <- bigstep t2
+bigstep (TOp t1 op t2) dec = do
+	VNum n1 <- bigstep t1 dec
+	VNum n2 <- bigstep t2 dec
 	return $ VNum (op n1 n2)
 
-bigstep (TIf t1 t2 t3) = do
-	VBool b <- bigstep t1
-	if b then bigstep t2 else bigstep t3
+bigstep (TIf t1 t2 t3) dec = do
+	VBool b <- bigstep t1 dec
+	if b then bigstep t2 dec else bigstep t3 dec
 
--- bigstep (TAbs v _ t) = Just (VFun v t)
+bigstep (TApp (TVar f) t) dec = do
+	VFun x t' <- Map.lookup f dec
+	flip bigstep dec $ subst x t t'
 
-bigstep (TApp t1 t2) = do
-	VFun x t <- bigstep t1
-	bigstep $ subst x t2 t
+bigstep (TApp _ _) _ = Nothing
 
-bigstep TEmpty = Just VEmpty
+bigstep TEmpty _ = Just VEmpty
 
-bigstep (TList t1 t2) = do
-	VNum n <- bigstep t1
-	case bigstep t2 of
+bigstep (TList t1 t2) dec = do
+	VNum n <- bigstep t1 dec
+	case bigstep t2 dec of
 		Just VEmpty -> Just $ VList (VNum n) VEmpty
 		Just l@(VList _ _) -> Just $ VList (VNum n) l
 		_ -> Nothing
 
-bigstep (TCase t1 t2 t3) =
-	case bigstep t1 of
-		Just VEmpty -> bigstep t2
-		Just (VList _ _) -> bigstep t3
+bigstep (TCase t1 t2 t3) dec =
+	case bigstep t1 dec of
+		Just VEmpty -> bigstep t2 dec
+		Just (VList _ _) -> bigstep t3 dec
 		_ -> Nothing
 
-bigstep _ = Nothing
+bigstep (TVar x) dec = Map.lookup x dec
 
 
 subst :: Var -> Term -> Term -> Term
@@ -68,7 +68,6 @@ subst _ TEmpty _ = TEmpty
 subst x (TList t1 t2) arg = TList (subst x t1 arg) (subst x t2 arg)
 subst x (TCase t1 t2 t3) arg = TCase (subst x t1 arg) (subst x t2 arg) (subst x t3 arg)
 subst x (TApp t1 t2) arg = TApp (subst x t1 arg) (subst x t2 arg)
--- subst x t@(TAbs y typeY body) arg = if x == y then t else TAbs y typeY (subst x body arg)
 
 data Type = TypeNat | TypeBool | TypeList | TypeFun Type Type deriving Eq
 
@@ -89,11 +88,6 @@ typecheck g (TIf t1 t2 t3) = do
 	if x == y then return x else Nothing
 
 typecheck g (TVar x) = g Map.!? x
-
--- typecheck g (TAbs x t body) = do
--- 	let g' = Map.insert x t g
--- 	t' <- typecheck g' body
--- 	return $ TypeFun t t'
 
 typecheck g (TApp t1 t2) = do
 	TypeFun type1 type2 <- typecheck g t1
