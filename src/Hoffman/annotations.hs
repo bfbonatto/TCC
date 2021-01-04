6eacb5276f48b28a60b75b354c80eeca7a92833e
Bruno Bonatto <bfbonatto@gmail.com>
2021-01-04 08:20:23 -0300

    type of typing judgement is still wrong

--- src/Hoffman/annotations.hs
+++ src/Hoffman/annotations.hs
@@ -3,17 +3,24 @@
 module Annotations where
 
 import Language
-import Typing
 import qualified Data.Map.Strict as Map
+
+data ResourceConstants =
+	KNil
+	| KBool
+	| KInt
+	| KOp
+	| KVar
+	| KConT1 | KConT2
+	| KConF1 | KConF2
+	| KApp1  | KApp2
+	| KCons
+	| KCaseN1 | KCaseN2 | KCaseC1 | KCaseC2 deriving Show
+
+rc2int _ = 1
 
 data A = ABool | AInt | AL Int A | Unit deriving Show
 data F = FFun A Int Int A deriving Show
 
 type AnnotatedContext = Map.Map Var A
 type FunctionContext = Map.Map Var F
-
-data AnnotatedTypingJudgement = ATJ Int A
-
-annotate :: FunctionContext -> AnnotatedContext -> Term -> Maybe AnnotatedTypingJudgement
-annotate = undefined
-
