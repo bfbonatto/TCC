2879c23f51a701edc464b2bdf814048834a0424b
Bruno Bonatto <bfbonatto@gmail.com>
2021-01-04 06:55:24 -0300

    solved an import cycle by merging language and typing modules

--- src/Hoffman/language.hs
+++ src/Hoffman/language.hs
@@ -2,7 +2,7 @@
 
 module Language where
 
-import Typing
+import qualified Data.Map.Strict as Map
 
 data Term = TBool Bool
 			| TNum Int
@@ -69,3 +69,46 @@ subst x (TList t1 t2) arg = TList (subst x t1 arg) (subst x t2 arg)
 subst x (TCase t1 t2 t3) arg = TCase (subst x t1 arg) (subst x t2 arg) (subst x t3 arg)
 subst x (TApp t1 t2) arg = TApp (subst x t1 arg) (subst x t2 arg)
 subst x t@(TAbs y typeY body) arg = if x == y then t else TAbs y typeY (subst x body arg)
+
+data Type = TypeNat | TypeBool | TypeList | TypeFun Type Type deriving Eq
+
+type Context = Map.Map Var Type
+
+typecheck :: Context -> Term -> Maybe Type
+typecheck _ (TBool _) = Just TypeBool
+typecheck _ (TNum _) = Just TypeNat
+typecheck g (TOp t1 _ t2) = do
+	TypeNat <- typecheck g t1
+	TypeNat <- typecheck g t2
+	return TypeNat
+
+typecheck g (TIf t1 t2 t3) = do
+	TypeBool <- typecheck g t1
+	x <- typecheck g t2
+	y <- typecheck g t3
+	if x == y then return x else Nothing
+
+typecheck g (TVar x) = g Map.!? x
+
+typecheck g (TAbs x t body) = do
+	let g' = Map.insert x t g
+	t' <- typecheck g' body
+	return $ TypeFun t t'
+
+typecheck g (TApp t1 t2) = do
+	TypeFun type1 type2 <- typecheck g t1
+	t <- typecheck g t2
+	if type1 == t then return type2 else Nothing
+
+typecheck _ TEmpty = Just TypeList
+
+typecheck g (TList t1 t2) = do
+	TypeNat <- typecheck g t1
+	TypeList <- typecheck g t2
+	return TypeList
+
+typecheck g (TCase t1 t2 t3) = do
+	TypeList <- typecheck g t1
+	type2 <- typecheck g t2
+	type3 <- typecheck g t3
+	if type2 == type3 then return type2 else Nothing
